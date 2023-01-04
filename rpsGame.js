64587178a3bf4cda510f8179553847bf1d5cafe6
Miklós Joánovits <72404757+majkulmajkul@users.noreply.github.com>
2023-01-04 20:40:37 +0100

    getPlayersChoice

--- rpsGame.js
+++ rpsGame.js
@@ -1,8 +1,20 @@
+const choices = ["rock", "paper", "scissors"]
+
 function getComputerChoice() {
 
-    const choices = ["rock", "paper", "scissors"]
+    
     const randomChoice = Math.floor(Math.random() * 3)
 
     return choices[randomChoice]
+}
+
+function getPlayersChoice() {
+    
+    let choice = ""
+    while (!choices.includes(choice)) {
+        choice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    }
+
+    return choice
 }
 
