4267550baa1735c107959d615992ca1509569be0
Miklós Joánovits <72404757+majkulmajkul@users.noreply.github.com>
2023-01-04 21:12:59 +0100

    Refractor get players choice

--- rpsGame.js
+++ rpsGame.js
@@ -1,4 +1,4 @@
-const choices = ["rock", "paper", "scissors"]
+const choices = [{name:"rock", beats:"scissors"}, {name:"paper", beats:"rock"}, {name:"scissors", beats:"rock"}]
 
 function getComputerChoice() {
 
@@ -11,10 +11,26 @@ function getComputerChoice() {
 function getPlayersChoice() {
     
     let choice = ""
-    while (!choices.includes(choice)) {
+    while (!choices.map(choice => choice.name).includes(choice)) {
         choice = prompt("Rock, Paper or Scissors?").toLowerCase();
     }
 
     return choice
 }
 
+function decide(computerChoice, playerChoice) {
+    
+    let winner = ""
+    
+    const theseChoices = [{name:"Computer", choice:computerChoice}, {name:"Player", choice:playerChoice}]
+
+
+
+
+
+
+}
+
+//console.log(decide(getComputerChoice(), getPlayersChoice()))
+
+console.log(getPlayersChoice())
