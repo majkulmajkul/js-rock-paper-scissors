d65ec2d252da72b74c5c867ed898766f055881b6
Miklós Joánovits <72404757+majkulmajkul@users.noreply.github.com>
2023-01-04 20:09:23 +0100

    Added getComputerChoice

--- rpsGame.js
+++ rpsGame.js
@@ -1,1 +1,8 @@
-console.log("All OK!")
+function getComputerChoice() {
+
+    const choices = ["rock", "paper", "scissors"]
+    const randomChoice = Math.floor(Math.random() * 3)
+
+    return choices[randomChoice]
+}
+
